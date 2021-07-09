43b00ab63ce25da423528b76d96d126dc8e4fb06
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-07-09 09:21:20 -0400

    initial commit, setup project for tailwind, firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import 'firebase/analytics';
+
+// Add the Firebase products that you want to use
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC6kDthry3jyCTVT5fTAmmoNWNiY5P6-wM',
+  authDomain: 'split-the-bill-wilsonle.firebaseapp.com',
+  projectId: 'split-the-bill-wilsonle',
+  storageBucket: 'split-the-bill-wilsonle.appspot.com',
+  messagingSenderId: '1013787776558',
+  appId: '1:1013787776558:web:3df28f655cdc670150f1bf',
+  measurementId: 'G-ZKVXW3YGHB',
+});
+
+export { firebase };
