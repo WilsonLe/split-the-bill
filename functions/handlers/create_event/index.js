842d90b59d5e3a32f015f5ba77ae17d67b20c4a8
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-07-16 08:43:31 -0400

    implement saving to db when creating event

--- functions/handlers/create_event/index.js
+++ functions/handlers/create_event/index.js
@@ -1,6 +1,17 @@
-const create_event = (data, context) => {
-  
-  return JSON.stringify(data);
+const { v4: uuidv4 } = require("uuid");
+const db = require("../../firebase.config");
+
+const create_event = async (data, context) => {
+  const name = `${data.name} (${data.email})`;
+  const event = {
+    creator: name,
+    code: uuidv4(),
+    members: [name],
+    expenses: {},
+  };
+  event["members"].forEach((member) => (event["expenses"][member] = 0));
+  const res = await db.collection("events").add(event);
+  return JSON.stringify(res);
 };
 
 module.exports = create_event;
