570f63f61f03860490221119ff2fc3db77b39fe7
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-07-19 19:42:32 -0400

    boilerplate code for Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import Main from "./Pages/Main";
+
+const pages = [
+  {
+    path: "/",
+    component: Main,
+  },
+];
 
 const App = () => {
   return <div className="bg-gray-300">hello world</div>;
---
+++ frontend/src/Pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import Reac, { FC } from "react";
+
+interface props {}
+
+const Main: FC<props> = ({}) => {
+  return (
+    <>
+      <div>Main Page</div>
+    </>
+  );
+};
+
+export default Main;
