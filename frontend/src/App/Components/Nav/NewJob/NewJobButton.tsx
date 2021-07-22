96631849b344e896eb17b8802e1e03dfd160a90f
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-07-21 23:40:30 -0400

    implement semifunctional NewJobPrompt

--- frontend/src/App/Components/Nav/NewJob/NewJobButton.tsx
+++ frontend/src/App/Components/Nav/NewJob/NewJobButton.tsx
@@ -2,17 +2,21 @@ import React, { FC } from "react";
 import { PlusIcon } from "@heroicons/react/solid";
 import { Menu } from "@headlessui/react";
 
-interface Props {}
+interface Props {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const NewJobButton: FC<Props> = () => {
+const NewJobButton: FC<Props> = ({ open, setOpen }) => {
   return (
-    <Menu.Button
+    <button
       type="button"
+      onClick={() => setOpen(!open)}
       className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
     >
       <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
       <span>New Job</span>
-    </Menu.Button>
+    </button>
   );
 };
 export default NewJobButton;
--- frontend/src/App/Components/Nav/NewJob/index.tsx
+++ frontend/src/App/Components/Nav/NewJob/index.tsx
@@ -1,37 +1,33 @@
-import React, { FC, Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
+import React, { FC, Fragment, useRef, useState } from "react";
+import useOnClickOutside from "../../../../utils/useOnClickOutside";
 
 import NewJobButton from "./NewJobButton";
+import NewJobPrompt from "./NewJobPrompt";
 
 interface Props {}
 
 const NewJob: FC<Props> = () => {
+  const [open, setOpen] = useState(false);
+  const NewJobRef = useRef(null);
+  useOnClickOutside(NewJobRef, () => setOpen(false));
   return (
     <>
-      <div className="flex-shrink-0">
-        <Menu as="div" className="relative inline-block">
-          {({ open }) => (
-            <>
-              <NewJobButton />
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <Menu.Item>
-                    <div>HELLO</div>
-                  </Menu.Item>
-                </Menu.Items>
-              </Transition>
-            </>
-          )}
-        </Menu>
+      <div className="relative" ref={NewJobRef}>
+        <NewJobButton open={open} setOpen={setOpen} />
+        <Transition
+          show={open}
+          as="div"
+          className="absolute origin-top-right right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          enter="transition ease-out duration-200"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          {open && <NewJobPrompt />}
+        </Transition>
       </div>
     </>
   );
