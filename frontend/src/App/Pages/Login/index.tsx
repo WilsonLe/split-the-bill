270f3efe9b9d81d3e6d07d39ab3dc0ab2ad28e01
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-07-23 15:37:27 -0400

    implement boilerplate code for Main and Login

--- frontend/src/App/Pages/Login/index.tsx
+++ frontend/src/App/Pages/Login/index.tsx
@@ -2,6 +2,7 @@ import React, { FC, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import UserContext from "../../Contexts/UserContext";
 import { firebase } from "../../../firebase.config";
+
 interface Props {}
 
 const Login: FC<Props> = () => {
@@ -14,8 +15,56 @@ const Login: FC<Props> = () => {
 
   return (
     <>
-      <h1>Login Page</h1>
-      {user ? <Redirect to="/" /> : <button onClick={sign_in}>Sign In</button>}
+      <div className="max-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-white text-gray-500">
+                    Conntinue with
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-6 grid grid-cols-3 gap-3">
+                <div></div>
+
+                <div>
+                  <button className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                    <span className="sr-only">Sign in with Google</span>
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                    </svg>
+                  </button>
+                </div>
+
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      )
     </>
   );
 };
--- frontend/src/App/Pages/Main/index.tsx
+++ frontend/src/App/Pages/Main/index.tsx
@@ -1,9 +1,10 @@
 import React, { FC, useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 import { db, firebase } from "../../../firebase.config";
 
 import UserContext from "../../Contexts/UserContext";
+import EventList from "./EventList";
 
 interface Props {}
 
@@ -31,13 +32,16 @@ const Main: FC<Props> = () => {
 
   return (
     <>
-      <h1>Main Page</h1>
-      {user && <p>{user.uid}</p>}
-      <Link to="/event">To event</Link>
+      {!user && <Redirect to="/login" />}
+      <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Events</h3>
+      </div>
 
-      {creatorEvents.map((event: firebase.firestore.DocumentData) => (
+      <EventList />
+
+      {/* {creatorEvents.map((event: firebase.firestore.DocumentData) => (
         <div key={event.code}>{event.name}</div>
-      ))}
+      ))} */}
     </>
   );
 };
