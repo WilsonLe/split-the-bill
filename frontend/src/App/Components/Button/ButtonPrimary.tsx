2fb0ba2cdfd7314f44ac3fac32c375b4a36591b3
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-08-16 13:13:33 -0500

    moved props classname of ButtonPrimary down, so that parents classnames overwrites children's

--- frontend/src/App/Components/Button/ButtonPrimary.tsx
+++ frontend/src/App/Components/Button/ButtonPrimary.tsx
@@ -9,8 +9,8 @@ const ButtonPrimary: FC<
   return (
     <button
       {...props}
-      className={`
-        ${props.className} inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+      className={`inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 
+      ${props.className} `}
     >
       {children}
     </button>
