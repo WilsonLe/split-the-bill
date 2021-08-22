64abb4e5a10630703ae02f610bfc416d1606c3af
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-08-22 17:33:31 -0400

    attemp to split production and development projects 6.55

--- frontend/src/firebase.config.ts
+++ frontend/src/firebase.config.ts
@@ -30,6 +30,16 @@ firebase.initializeApp({
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
+console.log({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
 // firebase.functions().useEmulator("localhost", 5001);
 
 const db = firebase.firestore();
