c6f7447237694b5fbec9d93166d34708463281ad
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-08-22 15:16:42 -0400

    attemp to split production and development projects 6.0

--- frontend/src/firebase.config.ts
+++ frontend/src/firebase.config.ts
@@ -10,33 +10,27 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/functions";
 
-const productionConfig = {
-  apiKey: "AIzaSyBkdu7hJMC2YoFWdEQzt9kcAf__iloYKqU",
-  authDomain: "split-the-bill-production.firebaseapp.com",
-  projectId: "split-the-bill-production",
-  storageBucket: "split-the-bill-production.appspot.com",
-  messagingSenderId: "705905223142",
-  appId: "1:705905223142:web:73cc4b89d09e2d707aa227",
-  measurementId: "G-QZWPK08CE6",
-};
+// const productionConfig = {
+//   apiKey: "AIzaSyBkdu7hJMC2YoFWdEQzt9kcAf__iloYKqU",
+//   authDomain: "split-the-bill-production.firebaseapp.com",
+//   projectId: "split-the-bill-production",
+//   storageBucket: "split-the-bill-production.appspot.com",
+//   messagingSenderId: "705905223142",
+//   appId: "1:705905223142:web:73cc4b89d09e2d707aa227",
+//   measurementId: "G-QZWPK08CE6",
+// };
 
-const developmentConfig = {
-  apiKey: "AIzaSyC6kDthry3jyCTVT5fTAmmoNWNiY5P6-wM",
-  authDomain: "split-the-bill-wilsonle.firebaseapp.com",
-  projectId: "split-the-bill-wilsonle",
-  storageBucket: "split-the-bill-wilsonle.appspot.com",
-  messagingSenderId: "1013787776558",
-  appId: "1:1013787776558:web:3df28f655cdc670150f1bf",
-  measurementId: "G-ZKVXW3YGHB",
-};
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
 
-if (process.env.REACT_APP_DEPLOY_ENV === "production") {
-  firebase.initializeApp(productionConfig);
-} else if (process.env.REACT_APP_DEPLOY_ENV === "development") {
-  firebase.initializeApp(developmentConfig);
-}
-
-firebase.functions().useEmulator("localhost", 5001);
+// firebase.functions().useEmulator("localhost", 5001);
 
 const db = firebase.firestore();
 const auth = firebase.auth();
