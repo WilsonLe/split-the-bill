34b118b7ebae259b8a92ea1184464e9070c699c9
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-08-22 23:26:09 -0400

    attemp to split production and development projects 7

--- frontend/src/firebase.config.ts
+++ frontend/src/firebase.config.ts
@@ -10,27 +10,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/functions";
 
-// const productionConfig = {
-//   apiKey: "AIzaSyBkdu7hJMC2YoFWdEQzt9kcAf__iloYKqU",
-//   authDomain: "split-the-bill-production.firebaseapp.com",
-//   projectId: "split-the-bill-production",
-//   storageBucket: "split-the-bill-production.appspot.com",
-//   messagingSenderId: "705905223142",
-//   appId: "1:705905223142:web:73cc4b89d09e2d707aa227",
-//   measurementId: "G-QZWPK08CE6",
-// };
-
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-});
-
-console.log({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
