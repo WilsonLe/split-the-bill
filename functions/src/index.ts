a9287ab9fb85bd2e0c2670b3ca21f3ecdd209a47
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-11-03 10:30:57 -0400

    implement clean up routine function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,19 @@
 import * as functions from "firebase-functions";
+import { db } from "../firebase.config";
 
 exports.removeOldEvent = functions.pubsub
   .schedule("0 0 * * *")
   .timeZone("America/New_York")
-  .onRun((context) => {
-    console.log("runs everyday at midnight, NY timezone");
+  .onRun(async (context) => {
+    const daysBack = 90;
+    const now = new Date();
+    const cutoffDate = new Date().setDate(now.getDate() - daysBack);
+    const eventSnap = await db
+      .collection("events")
+      .where("createdAt", "<=", cutoffDate)
+      .get();
+    const batch = db.batch();
+    eventSnap.forEach((doc) => batch.delete(doc.ref));
+    await batch.commit();
+    console.log("Clean up");
   });
