3098bf2519fd467eafc6d14f4fbc05d9506c0389
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-11-04 10:59:57 -0400

    update some title, favicon, implement boiler plate code and script for end2end tests

--- test/jest-puppeteer.config.js
+++ test/jest-puppeteer.config.js
@@ -3,6 +3,7 @@ module.exports = {
     dumpio: true,
     headless: process.env.CI === "true",
     product: "chrome",
+    slowMo: process.env.CI === "true" ? 0 : 500,
   },
   browserContext: "default",
 };
---
+++ test/src/index.test.ts
@@ -0,0 +1,19 @@
+import "expect-puppeteer";
+
+describe("New User Flow", () => {
+  beforeAll(async () => {
+    await page.goto("http://localhost:3000");
+  });
+  it('should display "Sign in to your account"', async () => {
+    await expect(page).toMatch("Sign in to your account");
+  });
+  it("Should let new user sign up", async () => {
+    await expect(page).toClick("button");
+    const pages = await browser.pages();
+    const widget = pages[pages.length - 1];
+    expect(await widget.title()).toBe("Auth Emulator IDP Login Widget");
+    await expect(widget).toMatch("Sign-in with");
+    await expect(widget).toMatch("Google.com");
+    await expect(widget).toClick("button", { text: "Add New Account" });
+  });
+});
